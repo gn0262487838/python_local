1581c0c0b7918ab9057de85899ad1696635715fd
Chenkol_baby <49306125+gn0262487838@users.noreply.github.com>
2019-09-03 16:26:56 +0800

    webcrawler ex

---
+++ webcrawler_weather.py
@@ -0,0 +1,38 @@
+# -*- utf-8 -*- #
+from bs4 import BeautifulSoup 
+import requests
+import prettytable
+url = "https://www.cwb.gov.tw/V7/forecast/f_index.htm?_=1566803524459"
+
+response = requests.get(url)
+# 告訴回應編碼是utf8
+response.encoding = "utf8"
+html = BeautifulSoup(response.text)
+
+lis = html.find("div",style="width:670px; border:0px red solid; overflow:hidden; padding:0 0 0 20px;")
+print(lis)
+pt = prettytable.PrettyTable()
+c = []
+t = []
+p = []
+for i in lis.find_all("td",width="60%"):
+    c.append(i.find("a").text)
+for i in lis.find_all("td",width="50%"):
+    t.append(i.find("a").text)
+for i in lis.find_all("td",width="18%"):
+    p.append(i.find("a").text)
+
+pt.add_column("城市",c)
+pt.add_column("溫度",t)
+pt.add_column("降雨機率",p)
+print(pt)
+
+
+
+
+    
+
+
+
+    
+    
